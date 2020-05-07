ab1cf786db047494a304fa9ec9ed3b7cd8c9de0a
amrabdou28 <amrabdou24@gmail.com>
2020-05-07 03:57:25 +0200

    third add

--- main.js
+++ main.js
@@ -2,37 +2,54 @@
 let amount = document.getElementById ("amount");
 let quality = document.getElementById ("the_quality");
 let peopleNumber = document.getElementById ("number");
-
+let inputs =document.querySelectorAll ("input");
 
 
 //hide the results section
 document.getElementById("results").style.display = "none";
 
+
 class container{
-    //clculateBill  
+    //calculateBill  
     calculateBill(amount,quality,peopleNumber){
         var calculat = (this.amount / this.peopleNumber) * this.quality;
         calculat = Math.round(calculat * 100) / 100;
-    }
-    // add result to html
-    billResult(){
-        document.getElementById("result").innerHTML = "calculateBill";
+        return calculat();
+        
         
     }
+    
+    // add result to html
+    billResult(){
+        document.getElementById("result").innerHTML = this.calculat;
+        
+    }
+    resetForm(){
+        amount.value ="";
+        peopleNumber.value = "";
+    }
+
+    
+    
+        
+    
     chickInput(){
         document.getElementById('calcbtn').addEventListener('click',()=>{
             
             if(amount.value === "" || peopleNumber.value === ""){
-                alert('please fill input first!!')
+                alert('please fill input first!!');
+                peopleNumber.focus();
+                amount.focus();
+            
             }
             else if(amount.value <= 0 ||peopleNumber.value <= 0){
                 alert("Invaild value");
-                peopleNumber.focus();
-                amount.focus();
+                this.resetForm();
             }
             else{
                 this.billResult();
                 document.getElementById("results").style.display = "block";
+                this.resetForm();
 
             }
         })
